fd096922276e369a17971c5002f4399736ef0a46
Popa Andrei <andrei.popa.1002@gmail.com>
2015-06-22 19:37:44 +0100

    Added comments

--- JustEat/JustEat/DataSource/JERestaurantsDataSource.h
+++ JustEat/JustEat/DataSource/JERestaurantsDataSource.h
@@ -8,11 +8,19 @@
 
 #import <Foundation/Foundation.h>
 #import "JERestaurant.h"
-
+/**
+ *  This class will represent the restaurants list
+ *  All the operations on this list should be made by this class
+ */
 @interface JERestaurantsDataSource : NSObject
 
 @property (strong, nonatomic, readonly) NSArray *restaurants;
-
+/**
+ *  Method that will trigger the fetch of restaurants from the API
+ *
+ *  @param postCode   post code to be used in the fetch
+ *  @param completion block to be executed when the fetch is done
+ */
 - (void)loadRestaurantsListForPostCode:(NSString *)postCode withCompletion:(void(^)(NSError *error))completion;
 
 @end
--- JustEat/JustEat/DataSourceDelegates/JERestaurantsDataSourceDelegate.h
+++ JustEat/JustEat/DataSourceDelegates/JERestaurantsDataSourceDelegate.h
@@ -9,11 +9,19 @@
 #import <Foundation/Foundation.h>
 #import "JERestaurantsDataSource.h"
 @import UIKit;
-
+/**
+ *  This class will act as the data source delegate for a tableView that will display the restaurants
+ */
 extern NSString *const kRestaurantCellIdentifier;
 
 @interface JERestaurantsDataSourceDelegate : NSObject <UITableViewDataSource>
-
+/**
+ *  Initializes the delegate with a specific dataSource class
+ *
+ *  @param dataSource JERestaurantsDataSource object that does the fetch
+ *
+ *  @return fresh new initialized JERestaurantsDataSource object
+ */
 - (instancetype)initWithDataSource:(JERestaurantsDataSource *)dataSource;
 
 @end
