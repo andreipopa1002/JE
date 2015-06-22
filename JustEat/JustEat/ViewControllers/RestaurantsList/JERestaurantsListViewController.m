f9f76409da24acd28af8e0b36d1d92a613b14cc1
Popa Andrei <andrei.popa.1002@gmail.com>
2015-06-22 19:25:55 +0100

    used the new dataSource and dataSourceDelegate

--- JustEat/JustEat/ViewControllers/RestaurantsList/JERestaurantsListViewController.m
+++ JustEat/JustEat/ViewControllers/RestaurantsList/JERestaurantsListViewController.m
@@ -7,18 +7,16 @@
 //
 
 #import "JERestaurantsListViewController.h"
-#import "JEApiClient.h"
-#import "JERestaurant.h"
+#import "JERestaurantsDataSource.h"
+#import "JERestaurantsDataSourceDelegate.h"
 #import "JERestaurantTableViewCell.h"
-#import <AFNetworking/UIImageView+AFNetworking.h>
 
-static NSString *const kRestaurantCellIdentifier = @"restaurant";
-
-@interface JERestaurantsListViewController ()<UITableViewDataSource, UITableViewDelegate>
+@interface JERestaurantsListViewController ()<UITableViewDelegate>
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @property (strong, nonatomic, readonly) NSString *postCode;
-@property (strong, nonatomic) NSArray *restaurants;
+@property (strong, nonatomic, readwrite) JERestaurantsDataSource *dataSource;
+@property (strong, nonatomic, readwrite) JERestaurantsDataSourceDelegate *dataSourceDelegate;
 @end
 
 @implementation JERestaurantsListViewController
@@ -37,42 +35,27 @@ static NSString *const kRestaurantCellIdentifier = @"restaurant";
     [self showLoadingIndicator];
     [self setupTableView];
     JERestaurantsListViewController *__weak weakSelf = self;
-    [[JEApiClient sharedClient] retrieveRestaurantsList:^(NSArray *restaurantsArray) {
-        [weakSelf hideLoadingIndicator];
-        if (restaurantsArray.count > 0) {
-            weakSelf.restaurants = [JERestaurant listOfRestaurantsFromArray:restaurantsArray];
-            [weakSelf.tableView reloadData];
-        } else if (restaurantsArray) {
-            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"No restaurants for this post code!", nil)
-                                                            message:NSLocalizedString(@"Please make sure you provided the corect post code", nil)
-                                                           delegate:nil
-                                                  cancelButtonTitle:NSLocalizedString(@"OK", nil)
-                                                  otherButtonTitles:nil];
-            [alert show];
-        }
-    } forPostCode:self.postCode];
+    [self.dataSource loadRestaurantsListForPostCode:self.postCode
+                                     withCompletion:^(NSError *error) {
+                                         [weakSelf hideLoadingIndicator];
+                                         if (error) {
+                                             [[[UIAlertView alloc] initWithTitle:@"Error"
+                                                                        message:error.domain
+                                                                       delegate:nil
+                                                              cancelButtonTitle:@"ok"
+                                                               otherButtonTitles:nil] show];
+                                         }
+                                         [weakSelf.tableView reloadData];
+                                     }];
 }
 
 - (void)setupTableView {
     [self.tableView registerNib:[UINib nibWithNibName:kJERestaurantTableViewCellNibName bundle:nil] forCellReuseIdentifier:kRestaurantCellIdentifier];
+    self.dataSource = [JERestaurantsDataSource new];
+    self.dataSourceDelegate = [[JERestaurantsDataSourceDelegate alloc] initWithDataSource:self.dataSource];
+    self.tableView.dataSource = self.dataSourceDelegate;
 }
 
-#pragma  mark - UITableViewDataSource methods
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return self.restaurants.count;
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    JERestaurantTableViewCell *cell = (JERestaurantTableViewCell *)[self.tableView dequeueReusableCellWithIdentifier:kRestaurantCellIdentifier forIndexPath:indexPath];
-    JERestaurant *restaurant = self.restaurants[indexPath.row];
-    cell.nameLabel.text = restaurant.name;
-    [cell.logoImage setImageWithURL:[NSURL URLWithString:restaurant.logoURL] placeholderImage:[UIImage new]];
-    cell.cuisinesLabel.text = [restaurant cuisinesString];
-    cell.ratingLabel.text = [restaurant ratingString];
-    [cell isOpenNow:restaurant.isOpenNow];
-    
-    return cell;
-}
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
     return kJERestaurantTableViewCellHeight;
@@ -80,7 +63,7 @@ static NSString *const kRestaurantCellIdentifier = @"restaurant";
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     if (self.delegate && [self.delegate respondsToSelector:@selector(userDidSelectRestaurant:)]) {
-        [self.delegate userDidSelectRestaurant:self.restaurants[indexPath.row]];
+        [self.delegate userDidSelectRestaurant:self.dataSource.restaurants[indexPath.row]];
         [tableView deselectRowAtIndexPath:indexPath animated:YES];
     }
 }
